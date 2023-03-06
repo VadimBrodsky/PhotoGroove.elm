adeaf8fabdea2df1ee9f859cbee7f05dd590239e
Vadim Brodsky <VadimBrodsky@users.noreply.github.com>
2023-03-06 18:46:13 -0500

    Use Elm's auto currying to partly apply viewThumbnail

--- src/PhotoGroove.elm
+++ src/PhotoGroove.elm
@@ -13,7 +13,7 @@ view model =
         [ h1 [] [ text "Photo Groove" ]
         , div [ id "thumbnails" ]
             (List.map
-                (\photo -> viewThumbnail model.selectedUrl photo)
+                (viewThumbnail model.selectedUrl)
                 model.photos
             )
         , img
