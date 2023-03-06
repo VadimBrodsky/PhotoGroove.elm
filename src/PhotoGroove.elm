a65290a33a8bf18eabdb92c16a2e10568673e96e
Vadim Brodsky <VadimBrodsky@users.noreply.github.com>
2023-03-06 13:30:00 -0500

    Add model and connect it to view

--- src/PhotoGroove.elm
+++ src/PhotoGroove.elm
@@ -4,16 +4,27 @@ import Html exposing (div, h1, img, text)
 import Html.Attributes exposing (..)
 
 
+urlPrefix =
+    "http://elm-in-action.com/"
+
+
 view model =
     div [ class "context" ]
         [ h1 [] [ text "Photo Groove" ]
-        , div [ id "thumbnails" ]
-            [ img [ src "http://elm-in-action.com/1.jpeg" ] []
-            , img [ src "http://elm-in-action.com/2.jpeg" ] []
-            , img [ src "http://elm-in-action.com/3.jpeg" ] []
-            ]
+        , div [ id "thumbnails" ] (List.map viewThumbnail model)
         ]
 
 
+viewThumbnail thumb =
+    img [ src (urlPrefix ++ thumb.url) ] []
+
+
 main =
-    view "no model yet"
+    view initialModel
+
+
+initialModel =
+    [ { url = "1.jpeg" }
+    , { url = "2.jpeg" }
+    , { url = "3.jpeg" }
+    ]
